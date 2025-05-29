3dee929ade9efcc4739078adf3659dc39441efc9
paddyoneill <30272453+paddyoneill@users.noreply.github.com>
2025-05-29 15:49:25 +0100

    Add SSH daemon to slurmd container (#4)
    Includes supervisord config to spawn process
    
    Includes sssd for managing user auth

--- schedmd/slurm/24.11/ubuntu24.04/files/slurmd/usr/local/bin/entrypoint.sh
+++ schedmd/slurm/24.11/ubuntu24.04/files/slurmd/usr/local/bin/entrypoint.sh
@@ -5,7 +5,14 @@
 set -euo pipefail
 
 mkdir -p /run/slurm/
+mkdir -p /run/sshd/
+chmod 0755 /run/sshd/
+mkdir -p /run/dbus/
+rm -f /var/run/dbus/pid
 
 export SLURMD_OPTIONS="${SLURMD_OPTIONS:-} $*"
+export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
+export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
+export DBUS_OPTIONS="${DBUS_OPTIONS:-""}"
 
 exec supervisord -c /etc/supervisor/supervisord.conf
